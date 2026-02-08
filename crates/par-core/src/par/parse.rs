cf18727efb18ab458ec910dc6067ae40f5106206
faiface <faiface2202@gmail.com>
2026-02-08 21:13:44 +0100

    par_core: fix dependencies

--- crates/par-core/src/par/parse.rs
+++ crates/par-core/src/par/parse.rs
@@ -363,7 +363,6 @@ pub(crate) fn parse_module(
     })
 }
 
-#[cfg(feature = "playground")]
 pub fn parse_bytes(input: &str, file: &FileName) -> Option<Vec<u8>> {
     (literal_bytes_inner, winnow::combinator::eof)
         .parse_next(&mut Input::new(&lex(input, file)))
